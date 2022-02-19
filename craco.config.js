b8f2f19f72dffe6dba9e873a494a56928eb48847
ecosse3 <luk.kurpiewski@gmail.com>
2022-02-19 14:47:26 +0100

    chore: add containers path to module resolver & extend interfaces

--- craco.config.js
+++ craco.config.js
@@ -17,7 +17,8 @@ module.exports = {
       '@mui/styled-engine': '@mui/styled-engine-sc',
       '#assets/*': path.resolve(__dirname, 'src/assets/*'),
       '#components/*': path.resolve(__dirname, 'src/components/*'),
-      '#interfaces': path.resolve(__dirname, 'src/interfaces'),
+      '#containers/*': path.resolve(__dirname, 'src/containers/*'),
+      '#interfaces/*': path.resolve(__dirname, 'src/interfaces/*'),
       '#store': path.resolve(__dirname, 'src/store'),
       '#store/*': path.resolve(__dirname, 'src/store/*'),
       '#utils/*': path.resolve(__dirname, 'src/utils/*')
