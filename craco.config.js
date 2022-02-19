0a5999a10b7049b50f58a7fadbeb45326867c47e
ecosse3 <luk.kurpiewski@gmail.com>
2022-02-19 11:30:55 +0100

    chore: extend resolver paths

--- craco.config.js
+++ craco.config.js
@@ -15,7 +15,11 @@ module.exports = {
   webpack: {
     alias: {
       '@mui/styled-engine': '@mui/styled-engine-sc',
-      '#store': path.resolve(__dirname, 'src/store')
+      '#components/*': path.resolve(__dirname, 'src/components/*'),
+      '#interfaces': path.resolve(__dirname, 'src/interfaces'),
+      '#store': path.resolve(__dirname, 'src/store'),
+      '#store/*': path.resolve(__dirname, 'src/store/*'),
+      '#utils/*': path.resolve(__dirname, 'src/utils/*')
     }
   }
 };
