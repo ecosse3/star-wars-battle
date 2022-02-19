a3fd1356c33678f2e502d87e8658c17266034439
ecosse3 <luk.kurpiewski@gmail.com>
2022-02-19 11:31:00 +0100

    chore: add more eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -54,6 +54,14 @@ module.exports = {
       }
     ],
     'object-curly-spacing': ['warn', 'always'],
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.tsx'] }]
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function'
+      }
+    ],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.tsx'] }],
+    'react/require-default-props': 0
   }
 };
