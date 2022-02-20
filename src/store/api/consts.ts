f0beca30bc134f028ad8c8e4b3fbfbc66a5128fe
ecosse3 <luk.kurpiewski@gmail.com>
2022-02-21 00:14:11 +0100

    fix(store): change base url because of not-working API

--- src/store/api/consts.ts
+++ src/store/api/consts.ts
@@ -1,1 +1,5 @@
-export const BASE_URL = 'https://swapi.dev/api';
+// I had to change this BASE_URL to one below because from
+// from Sunday (20/02) it is not longer available
+// https://github.com/phalt/swapi/issues/163
+// export const BASE_URL = 'https://swapi.dev/api';
+export const BASE_URL = 'https://swapi.py4e.com/api';
