e4413edc1940437ab8aa68724356e0c2993e4147
ecosse3 <luk.kurpiewski@gmail.com>
2022-02-21 00:15:22 +0100

    test: change TopBar color in test

--- src/components/TopBar/index.test.tsx
+++ src/components/TopBar/index.test.tsx
@@ -18,11 +18,10 @@ test('renders top bar component with logo', () => {
   expect(logo).toBeInTheDocument();
 });
 
-test('renders top bar component with black logo in light mode', () => {
+test('renders top bar component with amber logo in light mode', () => {
   renderComponent(<TopBar />);
-  useSelectorMock.mockReturnValue({ darkMode: false });
   const logo = screen.getByTestId('logo');
   const path = screen.getByTestId('logo-path');
   expect(logo).toBeInTheDocument();
-  expect(path).toHaveAttribute('fill', 'black');
+  expect(path).toHaveAttribute('fill', '#ffc107');
 });
