82164223bd32541017a67532af25ea932431e2b1
ecosse3 <luk.kurpiewski@gmail.com>
2022-02-21 22:07:29 +0100

    fix(store): change base url to swapi again

--- src/store/api/consts.ts
+++ src/store/api/consts.ts
@@ -1,5 +1,1 @@
-// I had to change this BASE_URL to one below because from
-// from Sunday (20/02) it is not longer available
-// https://github.com/phalt/swapi/issues/163
-// export const BASE_URL = 'https://swapi.dev/api';
-export const BASE_URL = 'https://swapi.py4e.com/api';
+export const BASE_URL = 'https://swapi.dev/api';
